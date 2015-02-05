1cd2ce63102a521b1a0b736ade09f44c1393d988
Ryan Ayers <rayers@dividia.net>
2015-02-05 08:32:12 +0000

    Bug 35509 - DVR not recording sporatically
    monarch
    - Filter out non maint customers from even logging to database.
    
    
    git-svn-id: http://develop.dividia.net/svn/repos/trunk@4816 4bbe417b-3d05-0410-9c88-dcef4dd82c0e

--- backend/modules/dvslog.py
+++ backend/modules/dvslog.py
@@ -6,7 +6,7 @@
 # Includes
 #
 # General
-import os, time
+import os, time, re
 # Logging
 from lib.messaging import stdMsg, dbgMsg, errMsg
 # Internal
@@ -15,63 +15,59 @@ import lib.cache
 
 class DVSLog:
     
-    def __init__( self ):
-        """ Initialize DVSLog Module """
+	def __init__( self ):
+		""" Initialize DVSLog Module """
 
-        libCache = lib.cache.Cache()
-        self._dbDVSLogList = libCache.get( 'dbDVSLogList' )
-        self._dbServerList = libCache.get( 'dbServerList' )
-               
-               
-    def updateDVSLog(self, bSerial=None, bEventID=None, sData=None):
-        """ Update the Log """
-        try:
-            # Initialize default return value
-            rgoResult = [ bSerial, bEventID ]
+		libCache = lib.cache.Cache()
+		self._dbDVSLogList = libCache.get( 'dbDVSLogList' )
+		self._dbServerList = libCache.get( 'dbServerList' )
 
-            if sData == 'None': sData = None
-            if bEventID == 'None': bEventID = None
-                        
-            try:
-                bSerial = int( bSerial )
-            except ValueError:
-                return rgoResult
-            try:
-                if bEventID is not None:
-                    bEventID = int( bEventID )
-            except ValueError:
-                return rgoResult
-            
-    
-            # Verify the Serial is known
-            oServer = self._dbServerList.getServer( bSerial=bSerial )
-            if oServer is None:
-                raise Exception, 'unknown serial [%s]' % bSerial
-            
-            
-            # Update return information
-            rgoResult[ 1 ] = bEventID
-           
-            sTimeStamp = time.time();
-            
-            self._dbDVSLogList.addDVSLogEntry(bSerial, bEventID, sData, sTimeStamp)
+	def updateDVSLog(self, bSerial=None, bEventID=None, sData=None):
+		""" Update the Log """
+		try:
+			if sData == 'None': sData = None
+			if bEventID == 'None': bEventID = None
+				
+			try:
+				bSerial = int( bSerial )
+			except ValueError:
+				raise Exception( 'bad serial' )
+			try:
+				if bEventID is not None:
+				bEventID = int( bEventID )
+			except ValueError:
+				raise Exception( 'bad event id' )
 
-            #===================================================================
-            # oDVSLogEntry.setSerial(bSerial)
-            # oDVSLogEntry.setEventID(bEventID)
-            # oDVSLogEntry.setData(sData)
-            # oDVSLogEntry.setTimestamp( time.time() )
-            # 
-            # self._dbDVSLogList.setDVSLogEntry( oDVSLogEntry )
-            #===================================================================
-            
 
-            stdMsg( 'DVSLog Entry added for serial-[%3s] event-[%s]' % ( bSerial, bEventID ) )
+			# See if we should skip anything for this serial
+			oMatchTest = re.compile( '.*test.*' )           # Regex to skip any "test" systems
+			oServer = self._dbServerList.getServer( bSerial=bSerial )
+			if oServer is None:
+				return False # server does not exist?
+			if oServer.checkHasSkip():
+				return False
+			if oMatchTest.match( oServer.getCategories() ):
+				return False
+			if oServer.getMaintenance() == 'no':
+				return False
 
-            return rgoResult
+			sTimeStamp = time.time();
 
-        except Exception, e:
-            errMsg('error updating DVSLog [%s]' % e)
-            #raise Exception, "System error updating ip."
-            return rgoResult
+			self._dbDVSLogList.addDVSLogEntry(bSerial, bEventID, sData, sTimeStamp)
 
+			#===================================================================
+			# oDVSLogEntry.setSerial(bSerial)
+			# oDVSLogEntry.setEventID(bEventID)
+			# oDVSLogEntry.setData(sData)
+			# oDVSLogEntry.setTimestamp( time.time() )
+			# 
+			# self._dbDVSLogList.setDVSLogEntry( oDVSLogEntry )
+			#===================================================================
+
+			stdMsg( 'DVSLog Entry added for serial-[%3s] event-[%s]' % ( bSerial, bEventID ) )
+
+			return True
+
+		except Exception, e:
+			errMsg('error updating DVSLog [%s]' % e)
+			return False
